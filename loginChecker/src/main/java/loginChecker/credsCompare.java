3ba653969307b4bf5b8a35143c14f418c8149180
shilpabhandari <shilpabhandari@localhost>
2022-03-26 00:24:23 -0400

    a simple login page

---
+++ loginChecker/src/main/java/loginChecker/credsCompare.java
@@ -0,0 +1,47 @@
+package loginChecker;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class credsCompare extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+			
+			Cookie cookies[] = req.getCookies();
+			
+			PrintWriter out = res.getWriter();
+			String user = null; 
+			String pw = null ;
+			
+			//it is not recognizing password for some reason
+			for(Cookie c: cookies) {
+				if (c.getName().trim().equals("username")){
+					user= c.getValue().trim();
+					//System.out.println(c.getName());
+				}
+				if (c.getName().trim().equals("password")) {
+					pw = c.getValue().trim();
+					//System.out.println(c.getName());
+					
+				}
+		}
+			
+			
+			
+			
+			
+			if (user.equals("Shilpa") && (pw.equals("Bhandari"))) {	
+				out.println("The login was successful");
+			}
+			else {
+				out.println("The login was unsuccessful");
+			}
+	
+
+	}
+}
---
+++ loginChecker/src/main/java/loginChecker/loginCheckerServlet.java
@@ -0,0 +1,32 @@
+package loginChecker;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class loginCheckerServlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		String i = req.getParameter("username");
+		String j = req.getParameter("password");
+		
+		
+		//HttpSession session = req.getSession();
+		//session.setAttribute("k", k);
+		
+		
+		//cookie is used to send multiple attributes
+		Cookie username = new Cookie("username", i.toString());
+		Cookie password = new Cookie("password", j.toString());
+		res.addCookie(username);
+		res.addCookie(password);
+		
+		res.sendRedirect("credsCompare");
+	}
+}
