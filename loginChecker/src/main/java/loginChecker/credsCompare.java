c4a0b3572028ec42507d6fcc8eacded3095b1b91
Shilpa <parasianshilpa@gmail.com>
2022-04-04 18:55:46 -0400

    replacing the use of servlets with annotations

--- loginChecker/src/main/java/loginChecker/credsCompare.java
+++ loginChecker/src/main/java/loginChecker/credsCompare.java
@@ -3,11 +3,13 @@ package loginChecker;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/credsCompare")
 public class credsCompare extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
--- loginChecker/src/main/java/loginChecker/loginCheckerServlet.java
+++ loginChecker/src/main/java/loginChecker/loginCheckerServlet.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/login")
 public class loginCheckerServlet extends HttpServlet{
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
